4220c4ed5a5f40a3efea925739ecd270610c7565
Heyson Betancourt <146369401+heysonbr@users.noreply.github.com>
2024-01-17 17:11:42 +0000

    ac

--- src/app.js
+++ src/app.js
@@ -51,6 +51,15 @@ function updateCard() {
   card.style.height = heightInput ? `${heightInput}px` : "500px";
   valor.style.fontSize = widthInput ? `${widthInput / 3}px` : "150px";
 
+  //const button2 = document.querySelector("#button2");
+  //button2.addEventListener("click", peque);
+
+  //const button3 = document.querySelector("#button3");
+  //button3.addEventListener("click", peque);
+
+  //const button4 = document.querySelector("#button4");
+  //button4.addEventListener("click", peque);
+
   //const heart = document.querySelector(".heart");
   //const diamond = document.querySelector(".diamond");
   //const club = document.querySelector(".club");
@@ -60,6 +69,27 @@ function updateCard() {
   //diamond.style.fontSize = widthInput ? `${widthInput / 3}px` : "150px";
   // club.style.fontSize = widthInput ? `${widthInput / 3}px` : "150px";
   // spade.style.fontSize = widthInput ? `${widthInput / 3}px` : "150px";
+}
+
+function changeCardSize2() {
+  const button2 = document.querySelector("#button2");
+  card.style.width = "200px";
+  card.style.height = "300px";
+  button2.addEventListener("click", peque);
+}
+
+function changeCardSize3() {
+  const button3 = document.querySelector("#button3");
+  card.style.width = "200px";
+  card.style.height = "300px";
+  button3.addEventListener("click", peque);
+}
+
+function changeCardSize4() {
+  const button4 = document.querySelector("#button4");
+  card.style.width = "200px";
+  card.style.height = "300px";
+  button4.addEventListener("click", peque);
 }
 
 window.onload = () => {
